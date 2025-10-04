33a77ee41d20def98897d1ed565e95aa58886b88
Sreedhar Narla <narlasusa@gmail.com>
2025-10-04 05:47:19 -0700

    second commit

--- M3-assignments/js/03-coinflip.js
+++ M3-assignments/js/03-coinflip.js
@@ -1,22 +1,27 @@
-let randomNum = Math.round(random());
-let result;
+let randomNum = Math.random();
+let coinFlip; 
  
 
 // If the number is less than 0.5, it's 'Heads'. Otherwise, it's 'Tails'.
   if (randomNum < 0.5) {
-    result = 'Heads';
+    coinFlip = 'Heads';
   } else {
-    result = 'Tails';
+    coinFlip = 'Tails';
   }
  // alert("The coin landed on " + (randomNum ? 'Heads' : 'Tails'));
 
 let choice = prompt("Choose heads or tails:").toLowerCase();
-alert("The coin landed on " +result + ".");
+//alert("The coin landed on " +result + ".");
 
-if (result===choice) {
-    alert("You win!");
-} else {
-    alert("You lose!");
+if (coinFlip ==='heads' && choice==='heads' ) {
+    alert("The flip was heads and you chose heads ... You win!");
+} else if (coinFlip ==='heads' && choice==='tails' ) {
+    alert("The flip was heads and you chose tails ... You lose!");
 }
-
+else if (coinFlip === 'tails' && choice === 'heads  ') {
+    alert("The flip was tails and you chose heads ... You lose!");
+}
+else  {
+    alert("The flip was tails and you chose tails ... You win!");
+}
 
